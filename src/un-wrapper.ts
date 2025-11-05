0a5f45c152c588f0b0ebb168688296225a7920d1
aksxmtvnty9 <anush.kumar.1998@gmail.com>
2025-11-05 16:55:41 +0530

    `feat`: Add `unWrapper()` and it's supporting utils

--- src/un-wrapper.ts
+++ src/un-wrapper.ts
@@ -1,1 +1,37 @@
-export const unWrapper = () => {};
+export const getFields = (data: unknown) => {
+  if (typeof data === 'object' && data !== null && 'fields' in data) {
+    return data.fields;
+  }
+
+  return {} as Record<string, unknown>;
+};
+
+export const unWrapper = (response: any): Record<string, any> | undefined => {
+  const fields = getFields(response) as Record<string, any>;
+  const keysOfFields = Object.keys(fields);
+  const contentTypeName = response?.sys?.contentType?.sys?.id || '';
+  const contentType = contentTypeName || response?.sys?.type || '';
+
+  if (!contentType) {
+    return undefined;
+  }
+
+  let data: Record<string, any> = {};
+
+  keysOfFields.forEach((key: string) => {
+    if (Array.isArray(fields[key])) {
+      data[key] = fields[key].map((c: any) => unWrapper(c));
+    }
+
+    if (
+      !Array.isArray(fields[key]) &&
+      typeof fields[key] === 'object' &&
+      Object.keys(fields[key]).includes('sys') &&
+      Object.keys(fields[key]).includes('fields')
+    ) {
+      data[key] = unWrapper(fields[key]);
+    }
+  });
+
+  return { contentType, ...data };
+};
